14a4274f909c87287a2758b30c00aaae39edec2e
Carl Kelvin <carlkelvin8@github.com>
2026-07-24 16:29:01 +0800

    Remove Support Tickets and FAQ Articles from sidebar

--- app/Filament/Resources/FaqArticleResource/Pages/EditFaqArticle.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace App\Filament\Resources\FaqArticleResource\Pages;
-use App\Filament\Resources\FaqArticleResource;
-use Filament\Resources\Pages\EditRecord;
-class EditFaqArticle extends EditRecord { protected static string $resource = FaqArticleResource::class; }
--- app/Filament/Resources/FaqArticleResource/Pages/ListFaqArticles.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace App\Filament\Resources\FaqArticleResource\Pages;
-use App\Filament\Resources\FaqArticleResource;
-use Filament\Resources\Pages\ListRecords;
-class ListFaqArticles extends ListRecords { protected static string $resource = FaqArticleResource::class; }
