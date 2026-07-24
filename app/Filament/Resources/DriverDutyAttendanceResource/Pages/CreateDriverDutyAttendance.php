631ecec062a9e0fa473085f4ecff7f7a0b9cc916
Carl Kelvin <carlkelvin8@github.com>
2026-07-24 16:30:20 +0800

    Remove DriverDutyAttendance resource (table schema mismatch)

--- app/Filament/Resources/DriverDutyAttendanceResource/Pages/CreateDriverDutyAttendance.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace App\Filament\Resources\DriverDutyAttendanceResource\Pages;
-use App\Filament\Resources\DriverDutyAttendanceResource;
-use Filament\Resources\Pages\CreateRecord;
-class CreateDriverDutyAttendance extends CreateRecord { protected static string $resource = DriverDutyAttendanceResource::class; }
--- app/Filament/Resources/DriverDutyAttendanceResource/Pages/ListDriverDutyAttendances.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace App\Filament\Resources\DriverDutyAttendanceResource\Pages;
-use App\Filament\Resources\DriverDutyAttendanceResource;
-use Filament\Resources\Pages\ListRecords;
-class ListDriverDutyAttendances extends ListRecords { protected static string $resource = DriverDutyAttendanceResource::class; }
